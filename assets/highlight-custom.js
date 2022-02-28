262e2d8d4a2972c15b5d95d852c58e83e91d3eb1
Jeffrey Edison <jeffrey.edison@gmail.com>
2022-02-28 18:25:06 +0100

    b2c cleanup
    move files from older b2c and documentation website

---
+++ assets/highlight-custom.js
@@ -0,0 +1,8 @@
+//Ref: https://highlightjs.readthedocs.io/en/latest/index.html
+//Initialise highlight js on <pre></code> blocks
+// hljs.initHighlighting();
+function doHighlight() {
+    // console.log("calling hljs");
+    hljs.highlightAll();
+}
+doHighlight();
